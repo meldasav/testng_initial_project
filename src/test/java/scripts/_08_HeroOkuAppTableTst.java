80cbb445680c3f825c67c50131e549fe95595dc0
meldasav <gmeldag83@hotmail.com>
2022-06-15 21:17:29 -0500

    changes added

--- src/test/java/scripts/_08_HeroOkuAppTableTst.java
+++ src/test/java/scripts/_08_HeroOkuAppTableTst.java
@@ -1,8 +1,8 @@
 package scripts;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -24,16 +24,28 @@ public class _08_HeroOkuAppTableTst extends Base {
             System.out.println(tableHeading1.get(i).getText());
 
         }
-        System.out.println();
-        List<WebElement> table1Row1 = driver.findElements(By.xpath("//table[@id='table1']//tr[1]//td"));
+    }
+    
+
+    @Test(priority = 2, description = "Tables |first row")
+    public void testFirstRow() {
+        driver.get("https://the-internet.herokuapp.com/");
+        heroAppPage.clickOnLink("Sortable Data Tables");
+
+        List<WebElement> table1Row1 = driver.findElements(By.cssSelector("#table1>tbody>tr:nth-child(1)>td"));
+
         String[] table1Row1Text = {"Smith", "John", "jsmith@gmail.com", "$50.00", "http://www.jsmith.com", "edit delete"};
+
         for (int i = 0; i < table1Row1.size(); i++) {
             Assert.assertTrue(table1Row1.get(i).isDisplayed());
             Assert.assertEquals(table1Row1.get(i).getText(), table1Row1Text[i]);
             System.out.println("************" + table1Row1.get(i).getText() + "************");
 
         }
-        System.out.println();
+    }
+
+    @Test()
+    public void testSecondRow() {
         List<WebElement> table1ROw2 = driver.findElements(By.xpath("//table[@id='table1']//tr[2]//td"));
         String[] table1Row2Text = {"Bach", "Frank", "fbach@yahoo.com", "$51.00", "http://www.frank.com", "edit delete"};
         for (int i = 0; i < table1ROw2.size(); i++) {
@@ -41,17 +53,56 @@ public class _08_HeroOkuAppTableTst extends Base {
             Assert.assertEquals(table1ROw2.get(i).getText(), table1Row2Text[i]);
             System.out.println("************" + table1ROw2.get(i).getText() + "************");
         }
-        System.out.println();
 
-        List<WebElement> table1Column2 = driver.findElements(By.xpath("//table[@id='table1']//tr//td[2]"));
+    }
+
+    @Test(priority = 3, description = "Tables | 2nd Column")
+    public void testSecondColumn() {
+        driver.get("https://the-internet.herokuapp.com/");
+        heroAppPage.clickOnLink("Sortable Data Tables");
+
+        //    List<WebElement> table1Column2 = driver.findElements(By.xpath("//table[@id='table1']//tr//td[2]"));
+
+        List<WebElement> table1Column2 = driver.findElements(By.cssSelector("#table1>tbody td:nth-child(2))"));
         String[] table1Column2Text = {"John", "Frank", "Jason", "Tim"};
+
         for (int i = 0; i < table1Column2.size(); i++) {
             Assert.assertTrue(table1Column2.get(i).isDisplayed());
             Assert.assertEquals(table1Column2.get(i).getText(), table1Column2Text[i]);
             System.out.println("************" + table1Column2.get(i).getText() + "************");
         }
-        List<WebElement>allCells = driver.findElements(By.xpath("//table[@id='table1']//td"));
-        Assert.assertFalse(allCells.isEmpty(),"They are not Empty");
-
     }
+
+    @Test(priority = 4, description = "Tables | all cells")
+    public void testAllCell() {
+        driver.get("https://the-internet.herokuapp.com/");
+        heroAppPage.clickOnLink("Sortable Data Tables");
+
+        List<WebElement> allCells = driver.findElements(By.cssSelector("#table1 td"));
+        System.out.println(allCells.size()); // 24
+
+        for(WebElement element : allCells){
+            System.out.println(element.getText());
+            Assert.assertNotEquals(element.getText(), "");
+        }
+    }
+
+
 }
+
+
+//        List<WebElement> allCells = driver.findElements(By.xpath("//table[@id='table1']//td"));
+//        Assert.assertFalse(allCells.isEmpty(), "They are Empty");
+
+
+//        boolean value = false;
+//        for (WebElement element : allCells) {
+//            if (element.getText().toLowerCase().startsWith("j")) {
+//                value = true;
+//                break;
+//            }
+//        }
+//        Assert.assertTrue(value, "Not found");
+//
+//    }
+
